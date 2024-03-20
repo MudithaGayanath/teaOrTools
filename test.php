1e05cbb016c62378bbb08c1e4f3888d9a8f3c88f
Muditha Gayanath <mudithawijesigha@gmail.com>
2024-03-20 06:35:18 +0530

    test over

--- test.php
+++ test.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap.css">
-    <title>Document</title>
+    <title>Admin Panale</title>
 </head>
 <body>
 <nav class="navbar bg-body-tertiary navbar-expand-md  sticky-top">
