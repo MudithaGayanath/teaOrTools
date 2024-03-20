cc0909548a788b534677de7c4b0d9139b8444b61
Muditha Gayanath <mudithawijesigha@gmail.com>
2024-03-20 21:45:25 +0530

    test Chart

--- test.php
+++ test.php
@@ -4,41 +4,79 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap.css">
-    <title>Admin Panale</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <title>Document</title>
 </head>
 <body>
-<nav class="navbar bg-body-tertiary navbar-expand-md  sticky-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Tea or Tools </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Tea or Tools</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Manger Users</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Manger Products</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">History</a>
-          </li>
-          
-        </ul>
-       
+<div>
+    <div class="container">
+        <div class="row">
+  <div class=" col-12 col-lg-6  ">
+    <div class="card ">
+      <div class="card-body">
+        
+      <h5 class="card-title ">Special title treatment</h5>
+      <canvas id="myChart" class=" "></canvas>
       </div>
     </div>
   </div>
-</nav>
-<script src="bootstrap.bundle.js"></script>
+  
+  
+        </div>
+  
+</div>
+<script>
+  const ctx = document.getElementById('myChart');
+  const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+function months(config) {
+  var cfg = config || {};
+  var count = cfg.count || 12;
+  var section = cfg.section;
+  var values = [];
+  var i, value;
+
+  for (i = 0; i < count; ++i) {
+    value = MONTHS[Math.ceil(i) % 12];
+    values.push(value.substring(0, section));
+  }
+
+  return values;
+}
+  const labels = months({count: 12});
+
+
+new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: labels ,
+      datasets: [{
+        label: 'Sales',
+        data: [12, 19, 3, 5000, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        }
+      }
+    }
+  });
+</script>
 </body>
 </html>
