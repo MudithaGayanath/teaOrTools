c8ab34bdbffff7ef2c29b0aae8b5efd01d17a6ab
Muditha Gayanath <mudithawijesigha@gmail.com>
2024-03-20 06:14:53 +0530

    Add session to admin

--- adminLoginProcess2.php
+++ adminLoginProcess2.php
@@ -1,9 +1,11 @@
 <?php
+    session_start();
     if ( $_SERVER["REQUEST_METHOD"] == "POST"){
         include("connection.php");
         $vc = $_POST["vc"];
         $rs = Database::search("SELECT * FROM `admin` WHERE `v_code`='".$vc."'");
         if ( $rs -> num_rows == 1 ){
+            $_SESSION["a"] = $rs -> fetch_assoc();
             echo("done");
         }else {
             echo("Invalid Verification Code");
